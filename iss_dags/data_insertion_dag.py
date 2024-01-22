33d44159d04cfa16ed1e9ffec376b598f44baea7
DavidTchanturia <davit.tchanturia.1@btu.edu.ge>
2024-01-23 01:29:36 +0400

    refactor: use hook.insert_all and airflow variables for insertion

--- iss_dags/data_insertion_dag.py
+++ iss_dags/data_insertion_dag.py
@@ -1,7 +1,9 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.models.variable import Variable
+from airflow.providers.google.cloud.hooks.bigquery import BigQueryHook
 from datetime import datetime
+import ast
 
 dag = DAG(
     dag_id='insert_into_bigquery',
@@ -11,10 +13,24 @@ dag = DAG(
 )
 
 
-def insert_data(**kwargs):
-    fetched_data = Variable.get("FETCHED_DATA")
+def insert_data():
+    BIGQUERY_CONN_ID = Variable.get("BIGQUERY_CONN_ID")
+    PROJECT_ID = Variable.get("PROJECT_ID")
+    DATASET_ID = Variable.get("DATASET_ID")
+    TABLE_ID = Variable.get("TABLE_ID")
+    FETCHED_DATA = Variable.get("FETCHED_DATA")
 
-    print(fetched_data)
+    bigquery_conn_id = BIGQUERY_CONN_ID
+    hook = BigQueryHook(bigquery_conn_id)
+
+    data_dict = ast.literal_eval(FETCHED_DATA)  # had to use this because it looked like a dict but was a str
+
+    hook.insert_all(
+        project_id=PROJECT_ID,
+        dataset_id=DATASET_ID,
+        table_id=TABLE_ID,
+        rows=[data_dict]  # Wrap the single dictionary in a list
+    )
 
 
 insert_into_bigquery_task = PythonOperator(
