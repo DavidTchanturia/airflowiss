b6b51be30f36891d37dea83b2f3ea1d2a6b1cf55
DavidTchanturia <davit.tchanturia.1@btu.edu.ge>
2024-01-23 11:11:37 +0400

    refactor: create function to retrieve variables from airflow

--- iss_dags/data_insertion_dag.py
+++ iss_dags/data_insertion_dag.py
@@ -12,15 +12,23 @@ dag = DAG(
     catchup=False
 )
 
-
-def create_table_if_not_exists():
+def get_credentials():
     BIGQUERY_CONN_ID = Variable.get("BIGQUERY_CONN_ID")
     PROJECT_ID = Variable.get("PROJECT_ID")
     DATASET_ID = Variable.get("DATASET_ID")
     TABLE_ID = Variable.get("TABLE_ID")
+    FETCHED_DATA = Variable.get("FETCHED_DATA")
 
-    bigquery_conn_id = BIGQUERY_CONN_ID
-    hook = BigQueryHook(bigquery_conn_id)
+    credentials = {"BIGQUERY_CONN_ID": BIGQUERY_CONN_ID, "PROJECT_ID": PROJECT_ID,
+                   "DATASET_ID": DATASET_ID, "TABLE_ID": TABLE_ID, "FETCHED_DATA": FETCHED_DATA}
+
+    return credentials
+
+
+def create_table_if_not_exists():
+    credentials = get_credentials()
+
+    hook = BigQueryHook(credentials["BIGQUERY_CONN_ID"])
 
     schema = [
         {"name": "id", "type": "INTEGER", "mode": "REQUIRED"},
@@ -36,9 +44,9 @@ def create_table_if_not_exists():
     ]
 
     hook.create_empty_table(
-        project_id=PROJECT_ID,
-        dataset_id=DATASET_ID,
-        table_id=TABLE_ID,
+        project_id=credentials["PROJECT_ID"],
+        dataset_id=credentials["DATASET_ID"],
+        table_id=credentials["TABLE_ID"],
         schema_fields=schema,
     )
 
@@ -50,31 +58,28 @@ create_table_task = PythonOperator(
     dag=dag
 )
 
+
 def insert_data():
-    BIGQUERY_CONN_ID = Variable.get("BIGQUERY_CONN_ID")
-    PROJECT_ID = Variable.get("PROJECT_ID")
-    DATASET_ID = Variable.get("DATASET_ID")
-    TABLE_ID = Variable.get("TABLE_ID")
-    FETCHED_DATA = Variable.get("FETCHED_DATA")
+    credentials = get_credentials()
 
-    bigquery_conn_id = BIGQUERY_CONN_ID
-    hook = BigQueryHook(bigquery_conn_id)
+    hook = BigQueryHook(credentials["BIGQUERY_CONN_ID"])
 
-    data_dict = ast.literal_eval(FETCHED_DATA)  # had to use this because it looked like a dict but was a str
+    data_dict = ast.literal_eval(credentials["FETCHED_DATA"])  # had to use this because it looked like a dict but was a str
 
     hook.insert_all(
-        project_id=PROJECT_ID,
-        dataset_id=DATASET_ID,
-        table_id=TABLE_ID,
-        rows=[data_dict]  # Wrap the single dictionary in a list
+        project_id=credentials["PROJECT_ID"],
+        dataset_id=credentials["DATASET_ID"],
+        table_id=credentials["TABLE_ID"],
+        rows=[data_dict]
     )
 
 
 insert_into_bigquery_task = PythonOperator(
     task_id='insert_into_bigquery_task',
     python_callable=insert_data,
-    provide_context=True,  # This is needed to access the task instance in the function
+    provide_context=True,
     dag=dag
 )
 
 create_table_task >> insert_into_bigquery_task
+
